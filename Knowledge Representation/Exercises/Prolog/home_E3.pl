68248ee1eb6ca9603863247c4238160861e510a1
Nick <brandimax2@gmail.com>
2018-12-15 14:51:56 +0100

    add ex 4

--- Knowledge Representation/Exercises/Prolog/home_E3.pl
+++ Knowledge Representation/Exercises/Prolog/home_E3.pl
@@ -53,9 +53,10 @@ count_elem_acc([H|Lst],Lenght,Acc):-
 % Binary Trees
 %==================
 
-T1 = tree(a,tree(b,tree(d,nil,nil),tree(e,nil,nil)),tree(c,nil,tree(f,tree(g,nil,nil),nil))).
-% T2 = tree(a,nil,nil).
-% T3 = nil.
+% Some tree examples:
+% tree(a,tree(b,tree(d,nil,nil),tree(e,nil,nil)),tree(c,nil,tree(f,tree(g,nil,nil),nil))).
+% tree(a,nil,nil).
+% nil.
 
 %---------------------------------------------
 
@@ -82,15 +83,44 @@ member(M,tree(_,_,R)):- member(M,R).
 %---------------------------------------------
 
 %3. Write a PROLOG program which returns a list containing
-%all the nodes at a given depth D of a binary tree.
+% all the nodes at a given depth D of a binary tree.
+
+% first we write the implemnetation which calls the accumulator version
+% in this case the accumulator will be an indicator of the current depth.
+depth_check(D,Tree,Res):- depth_check_sub(D,Tree,Res,1).
+
+
+% if the current depth is equal to the chosen one, set Res to a list containing
+% element
+depth_check_sub(D,tree(Elem,_,_),[Elem],D).
+ 
+%The above function can also be implemented as follows:
+
+/*
+depth_check_sub(D,tree(Elem,_,_),Res,CurrentD):- 
+                CurrentD==D,
+                append([],[Elem],Res).
+*/
+
+% if we haven't reached the desired depth D
+depth_check_sub(D,tree(_,L,R),Res,CurrentD):- 
+                CurrentD \== D, % you can omit this (since the previous call has failed)
+                NewCurrentD is CurrentD+1, % increase the depth
+                depth_check_sub(D,L,ResL,NewCurrentD), % call the function for the left part
+                depth_check_sub(D,R,ResR,NewCurrentD),% call the function for the right part
+                append(ResL,ResR,Res). % append results from left and right
 
 
 
+                
 %---------------------------------------------
 
 
 %Write a predicate cbal_tree/2 to construct completely balanced binary
 %trees for a given number of nodes. The predicate should generate all
-%solutions via backtracking. Put the letter 'x' as information into 
-%all nodes of the tree.
+%solutions via backtracking. 
+
+cbal_tree(N,Tree):- cbal_tree_acc(N,Tree,tree(0,_,_)).
+
+%cbal_tree_acc(N,Tree,Acc). 
 
